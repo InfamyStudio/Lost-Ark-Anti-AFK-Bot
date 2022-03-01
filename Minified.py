1dfe7eb756e20317608126bce8cb078d2c30509f
Eaglescream <clemens.hinkler@googlemail.com>
2022-03-01 16:09:51 +0100

    get window location every input
    u can now move the window

--- Minified.py
+++ Minified.py
@@ -34,12 +34,12 @@ def TimeSetup():
 def RandomTimeGen(userTimeSettings):A=userTimeSettings;B=randint(A[0],A[1]);return B
 def TimeSleep(timeSleepSettings):B=timeSleepSettings;A=RandomTimeGen(B);print('Next Interaction Time: '+str(A)+' second(s) or '+str(A//60)+' minute(s) (Integer Division Not Accurate Minute Representation)!');time.sleep(A)
 def RandomLocationGen(resScreenSize):A=resScreenSize;B=randint(A[0],A[1]);C=randint(A[2],A[3]);return B,C
-def LAForeground():A=win32com.client.Dispatch('WScript.Shell');A.SendKeys('%');win32gui.SetForegroundWindow(hwnd)
+def LAForeground():A=win32com.client.Dispatch('WScript.Shell');A.SendKeys('%');win32gui.SetForegroundWindow(hwnd);time.sleep(0.5)
 def LALocation():
 	if LAWindowTitle:
 		if hwnd:A=win32gui.GetWindowRect(hwnd);B=A[0];C=A[1];D=A[2]-B;E=A[3]-C;return B,C,D,E
 		else:print('Window not found!')
-def MouseClick(retScreenSize):LAForeground();D=retScreenSize;A=RandomLocationGen(D);B=A[0];C=A[1];pyautogui.moveTo(B,C);pyautogui.click();print(_C+str(B)+','+str(C)+')')
+def MouseClick(retScreenSize):LAForeground();LALocation();D=retScreenSize;A=RandomLocationGen(D);B=A[0];C=A[1];pyautogui.moveTo(B,C);pyautogui.click();print(_C+str(B)+','+str(C)+')')
 def ButtonSetup():
 	A=['q','w','e','r','a','s','d','f'];print('The Default Button Click List Is:');print(A)
 	while _B:
@@ -58,7 +58,7 @@ def ButtonSetup():
 		except ValueError:print(_F)
 	return A,B
 def ButtonClick(buttonSetup):A=buttonSetup;LAForeground();C=A[0];D=A[1]+7;E=randint(0,D);B=C[E];pyautogui.press(B);print('Button Clicked: '+B)
-def screenshot(window_title=None):E=retScreenSize[6];F=retScreenSize[7];I=E+retScreenSize[8];J=F+retScreenSize[9];K,L,M,N=win32gui.GetWindowRect(hwnd);O=M-K;P=N-L;G=win32gui.GetWindowDC(hwnd);B=win32ui.CreateDCFromHandle(G);C=B.CreateCompatibleDC();A=win32ui.CreateBitmap();A.CreateCompatibleBitmap(B,O,P);C.SelectObject(A);R=windll.user32.PrintWindow(hwnd,C.GetSafeHdc(),0);H=A.GetInfo();Q=A.GetBitmapBits(_B);D=Image.frombuffer('RGB',(H['bmWidth'],H['bmHeight']),Q,'raw','BGRX',0,1);win32gui.DeleteObject(A.GetHandle());C.DeleteDC();B.DeleteDC();win32gui.ReleaseDC(hwnd,G);D=D.crop((E,F,I,J));return D
+def screenshot(window_title=None):LALocation();E=retScreenSize[6];F=retScreenSize[7];I=E+retScreenSize[8];J=F+retScreenSize[9];K,L,M,N=win32gui.GetWindowRect(hwnd);O=M-K;P=N-L;G=win32gui.GetWindowDC(hwnd);B=win32ui.CreateDCFromHandle(G);C=B.CreateCompatibleDC();A=win32ui.CreateBitmap();A.CreateCompatibleBitmap(B,O,P);C.SelectObject(A);R=windll.user32.PrintWindow(hwnd,C.GetSafeHdc(),0);H=A.GetInfo();Q=A.GetBitmapBits(_B);D=Image.frombuffer('RGB',(H['bmWidth'],H['bmHeight']),Q,'raw','BGRX',0,1);win32gui.DeleteObject(A.GetHandle());C.DeleteDC();B.DeleteDC();win32gui.ReleaseDC(hwnd,G);D=D.crop((E,F,I,J));return D
 def QueueDetection(retScreenSize):
 	I='queueDetectionScreenshot.png';E='Sit Tight!';D='~Waiting 10 Seconds To Reanylse Screen!';C='Still in Queue :(';A=retScreenSize;F=A[4];G=A[5];L=A[6];M=A[7];N=A[8];O=A[9]
 	while _B:
