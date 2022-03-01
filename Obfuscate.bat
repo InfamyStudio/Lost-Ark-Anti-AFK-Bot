8c885ae70655f5dbad4eb47adf469aa49cdd52eb
Eaglescream <34615811+Eaglescream@users.noreply.github.com>
2022-03-01 02:29:09 +0100

    Delete Obfuscate.bat

--- Obfuscate.bat
+++
@@ -1,8 +0,0 @@
-pyminify "Lost Ark Anti AFK "Bot.py > Minified.py
-pyminify "Lost Ark Anti AFK "Bot.py > "Lost Ark Anti AFK "Bot_Minified_Test.py
-fc "Lost Ark Anti AFK "Bot_Minified.py "Lost Ark Anti AFK "Bot_Minified_Test.py
-del "Lost Ark Anti AFK "Bot_Minified_Test.py
-pyarmor obfuscate "Lost Ark Anti AFK "Bot_Minified.py
-del Minified.spec
-move /Y dist\Minified.py Obfuscated.py
-rmdir /s /q dist
