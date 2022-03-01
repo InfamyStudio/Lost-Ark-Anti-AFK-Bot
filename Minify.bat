b40c62b7c75984edf011787509e3635217583783
Eaglescream <34615811+Eaglescream@users.noreply.github.com>
2022-03-01 02:29:16 +0100

    Delete Minify.bat

--- Minify.bat
+++
@@ -1,4 +0,0 @@
-pyminify "Lost Ark Anti AFK "Bot.py > Minified.py
-pyminify "Lost Ark Anti AFK "Bot.py > "Lost Ark Anti AFK "Bot_Minified_Test.py
-fc Minified.py "Lost Ark Anti AFK "Bot_Minified_Test.py
-del "Lost Ark Anti AFK "Bot_Minified_Test.py
